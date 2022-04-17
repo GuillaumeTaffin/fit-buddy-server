cbabd5777197ce9d83ceca06fbffb5a922a71305
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-04-17 19:18:13 -0400

    first commit

---
+++ src/main/kotlin/gt/com/Application.kt
@@ -0,0 +1,14 @@
+package gt.com
+
+import io.ktor.server.application.*
+import gt.com.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSecurity()
+    configureSerialization()
+}
