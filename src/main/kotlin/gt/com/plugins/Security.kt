24bf75a3923f4f37f82d4cf201f928333c01252e
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2022-04-17 19:36:39 -0400

    first commit

--- src/main/kotlin/gt/com/plugins/Security.kt
+++ src/main/kotlin/gt/com/plugins/Security.kt
@@ -1,26 +1,26 @@
 package gt.com.plugins
 
 import io.ktor.server.auth.*
-import io.ktor.util.*
 import io.ktor.server.auth.jwt.*
 import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureSecurity() {
 
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwtRealm = environment.config.property("jwt.realm")
+    val jwtDomain = environment.config.property("jwt.domain")
+
     authentication {
         jwt {
-            val jwtAudience = environment.config.property("jwt.audience").getString()
-            realm = environment.config.property("jwt.realm").getString()
+
+           this.realm = jwtRealm.getString()
             verifier(
                 JWT
                     .require(Algorithm.HMAC256("secret"))
                     .withAudience(jwtAudience)
-                    .withIssuer(environment.config.property("jwt.domain").getString())
+                    .withIssuer(jwtDomain.getString())
                     .build()
             )
             validate { credential ->
